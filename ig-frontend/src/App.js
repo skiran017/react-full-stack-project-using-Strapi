d37d55001811f8ec5c0a4fc0c1a845b9a3046cb2
skiran017 <skiran017@gmail.com>
2021-08-02 01:03:22 +0530

    Hooks and Async function
    -useState and useEffect
    -async function in useEffect to load data

--- ig-frontend/src/App.js
+++ ig-frontend/src/App.js
@@ -1,7 +1,8 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './components/Post';
 
-const posts = [
+const mockPosts = [
   {
     likes: 7,
     description: 'Exploring',
@@ -26,17 +27,29 @@ const posts = [
 ];
 
 function App() {
-  return posts.map((post) => {
-    return (
-      <div className="App">
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await fetch('http://localhost:1337/posts');
+      const data = await response.json();
+      setPosts(data);
+    };
+
+    getPosts();
+  }, []);
+
+  return (
+    <div className="App">
+      {posts.map((post) => (
         <Post
           likes={post.likes}
           description={post.description}
-          url={post.image && post.image.url}
+          url={post.image && post.image[0].url}
         />
-      </div>
-    );
-  });
+      ))}
+    </div>
+  );
 }
 
 export default App;
